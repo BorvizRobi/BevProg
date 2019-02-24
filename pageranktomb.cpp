eda09d7dc191090a337e69a4794b9fe527374837
BorvizRobi <43295711+BorvizRobi@users.noreply.github.com>
2019-02-24 21:57:52 +0100

    Update pageranktomb.cpp

--- pageranktomb.cpp
+++ pageranktomb.cpp
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <math.h>
 
-void
-kiir (double tomb[], int db)
-{
-int i;
-for (i=0; i<db; i++)
-printf("PageRank [%d]: %lf\n", i, tomb[i]);
-}
+void kiir (double tomb[], int db)
+  {
+  
+  int i;
+  for (i=0; i<db; i++)
+  printf("PageRank [%d]: %lf\n", i, tomb[i]);
+  
+  }
 
 double tavolsag(double pagerank[],double pagerank_temp[],int db)
 {
@@ -20,35 +21,37 @@ return tav;
 
 int main(void)
 {
-double L[4][4] = {
-{0.0, 0.0, 1.0 / 3.0, 0.0},
-{1.0, 1.0 / 2.0, 1.0 / 3.0, 1.0},
-{0.0, 1.0 / 2.0, 0.0, 0.0},
-{0.0, 0.0, 1.0 / 3.0, 0.0}
-};
+  double L[4][4] = {
+  {0.0, 0.0, 1.0 / 3.0, 0.0},
+  {1.0, 1.0 / 2.0, 1.0 / 3.0, 1.0},
+  {0.0, 1.0 / 2.0, 0.0, 0.0},
+  {0.0, 0.0, 1.0 / 3.0, 0.0}
+  };
 
-double PR[4] = {0.0, 0.0, 0.0, 0.0};
-double PRv[4] = {1.0 / 4.0, 1.0 / 4.0, 1.0 / 4.0, 1.0 / 4.0};
+  double PR[4] = {0.0, 0.0, 0.0, 0.0};
+  double PRv[4] = {1.0 / 4.0, 1.0 / 4.0, 1.0 / 4.0, 1.0 / 4.0};
 
-long int i,j,h;
-i=0; j=0; h=5;
+  int i,j;
 
-for (;;)
-{
-for(i=0;i<4;i++)
-PR[i] = PRv[i];
-for (i=0;i<4;i++)
-{
-double temp=0;
-for (j=0;j<4;j++)
-temp+=L[i][j]*PR[j];
-PRv[i]=temp;
-}
 
-if ( tavolsag(PR,PRv, 4) < 0.00001)
-break;
-}
-kiir (PR,4);
-return 0;
+  for (;;)
+  {
+
+  for (i=0;i<4;i++)
+    {
+    PR[i]=0.0;
+    for (j=0;j<4;j++)
+      PR[i]+=L[i][j]*PRv[j];
+    }
+
+    if ( tavolsag(PR,PRv, 4) < 0.0000001)
+    break;
+  
+    for(i=0;i<4;i++)
+      PRv[i] = PR[i];
+  
+  }
+  kiir (PR,4);
+  return 0;
 
 } 
